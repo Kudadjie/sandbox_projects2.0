8d53798a4c41977e64191160214403f4c714b355
Kudadjie <johnkudadjiekabutetteh@outlook.com>
2021-08-13 13:06:44 +0000

    new features

--- bash_scripts/dm-stats.sh
+++ bash_scripts/dm-stats.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# max: threshold % of disk usage
+
 source functions_for_dm_stat
+MAX=90
 
 echo "      Disk Space / Memory Checker"
 
@@ -11,12 +14,20 @@ echo "      *******************************************
       *******************************************"
 echo " "
 
-echo "      *******************************************
+if [ $(percent_space_used) -ge $MAX ]; then
+    echo -e "      *******************************************
+      Used disk space: "$(used_space)"  
+      Free disk space: "$(free_space)" 
+      % of disk space used: "$(percent_space_used)" 
+      Total disk space available: "$(av_space)" 
+      "$(tput setaf 1)"WARNING: RUNNING OUT OF DISK SPACE!"$(tput sgr 0)"
+      *******************************************"
+else
+    echo "      *******************************************
       Used disk space: "$(used_space)"  
       Free disk space: "$(free_space)" 
       % of disk space used: "$(percent_space_used)"
       Total disk space available: "$(av_space)" 
       *******************************************"
-
-
+fi
 
